cf6ecc94e1663de501cc5ca434ed65d0dae023d4
alexxuyao <450335224@qq.com>
2016-09-15 17:42:25 +0800

    修改：     pipeline.go

--- pipeline.go
+++ pipeline.go
@@ -7,27 +7,27 @@
 package pipeline
 
 import (
-    "bytes"
-    "errors"
-    "fmt"
-    "os/exec"
-    "syscall"
+	"bytes"
+	"errors"
+	"fmt"
+	"os/exec"
+	"syscall"
 )
 
 const ErrCodeNil int = 1000
 
 type Error struct {
-    Code int
-    Err  error
+	Code int
+	Err  error
 }
 
 // implement interface error
 func (e *Error) Error() string {
-    return e.Err.Error()
+	return e.Err.Error()
 }
 
 func newErr(code int, format string, a ...interface{}) *Error {
-    return &Error{code, errors.New(fmt.Sprintf(format, a...))}
+	return &Error{code, errors.New(fmt.Sprintf(format, a...))}
 }
 
 /* Run strings together the given exec.Cmd commands in a similar fashion to the
@@ -63,60 +63,60 @@ func newErr(code int, format string, a ...interface{}) *Error {
         stdout, _, _ := pipeline.Run(&cmd, exec.Command("tr", "a-z", "A-Z"))
 */
 func Run(cmds ...*exec.Cmd) (stdout, stderr *bytes.Buffer, finErr error) {
-    // Require at least one command
-    if len(cmds) < 1 {
-        finErr = nil
-        return
-    }
+	// Require at least one command
+	if len(cmds) < 1 {
+		finErr = nil
+		return
+	}
 
-    stdout = new(bytes.Buffer)
-    stderr = new(bytes.Buffer)
-    finErr = nil
+	stdout = new(bytes.Buffer)
+	stderr = new(bytes.Buffer)
+	finErr = nil
 
-    last := len(cmds) - 1
-    for i, cmd := range cmds[:last] {
-        var err error
-        // Connect each command's stdin to the previous command's stdout
-        if cmds[i+1].Stdin, err = cmd.StdoutPipe(); err != nil {
-            finErr = &Error{ErrCodeNil, err}
-            return
-        }
-        // Connect each command's stderr to a buffer
-        cmd.Stderr = stderr
-    }
+	last := len(cmds) - 1
+	for i, cmd := range cmds[:last] {
+		var err error
+		// Connect each command's stdin to the previous command's stdout
+		if cmds[i+1].Stdin, err = cmd.StdoutPipe(); err != nil {
+			finErr = &Error{ErrCodeNil, err}
+			return
+		}
+		// Connect each command's stderr to a buffer
+		cmd.Stderr = stderr
+	}
 
-    // Connect the output and error for the last command
-    cmds[last].Stdout = stdout
-    cmds[last].Stderr = stderr
+	// Connect the output and error for the last command
+	cmds[last].Stdout = stdout
+	cmds[last].Stderr = stderr
 
-    // Start each command
-    for i, cmd := range cmds {
-        if err := cmd.Start(); err != nil {
-            finErr = newErr(ErrCodeNil, "start cmd[%v] failed: %v", i, err)
-            return
-        }
-    }
+	// Start each command
+	for i, cmd := range cmds {
+		if err := cmd.Start(); err != nil {
+			finErr = newErr(ErrCodeNil, "start cmd[%v] failed: %v", i, err)
+			return
+		}
+	}
 
-    // Wait for each command(in reverse order) to complete
-    for i := last; i >= 0; i -= 1 {
-        cmd := &cmds[i]
-        if err := cmd.Wait(); err != nil {
-            if exiterr, ok := err.(*exec.ExitError); ok {
-                // The program has exited with an exit code != 0
-                if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-                    finErr = newErr(status.ExitStatus(),
-                        "cmd[%v] exits %v", i, status.ExitStatus())
-                    return
-                } else {
-                    finErr = newErr(ErrCodeNil, "get exit_code failed for cmd[%v]", i)
-                    return
-                }
-            }
-            finErr = newErr(ErrCodeNil, "cmd[%v] failed: %v", i, err)
-            return
-        }
-    } // for
+	// Wait for each command(in reverse order) to complete
+	for i := last; i >= 0; i -= 1 {
+		cmd := cmds[i]
+		if err := cmd.Wait(); err != nil {
+			if exiterr, ok := err.(*exec.ExitError); ok {
+				// The program has exited with an exit code != 0
+				if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+					finErr = newErr(status.ExitStatus(),
+						"cmd[%v] exits %v", i, status.ExitStatus())
+					return
+				} else {
+					finErr = newErr(ErrCodeNil, "get exit_code failed for cmd[%v]", i)
+					return
+				}
+			}
+			finErr = newErr(ErrCodeNil, "cmd[%v] failed: %v", i, err)
+			return
+		}
+	} // for
 
-    // Return the pipeline output and the collected standard error
-    return
+	// Return the pipeline output and the collected standard error
+	return
 }
